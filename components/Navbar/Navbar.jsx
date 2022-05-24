70440800faba990fe8ab3545e28b081243c36584
raihanmuhammad <michael.raihan@stockbit.com>
2022-05-24 20:23:52 +0700

    fix: background header and border navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -3,32 +3,34 @@ import styles from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
-      <div>
-        <img
-          src="/images/logo.svg"
-          alt="Logo Netbook"
-          className={styles.logo}
-        />
-        <ul>
-          <li>
-            <Link href="">Home</Link>
-          </li>
-          <li>
-            <Link href="">Community</Link>
-          </li>
-          <li>
-            <Link href="">Blog</Link>
-          </li>
-          <li>
-            <Link href="">Events</Link>
-          </li>
-        </ul>
-      </div>
+      <section>
+        <div>
+          <img
+            src="/images/logo.svg"
+            alt="Logo Netbook"
+            className={styles.logo}
+          />
+          <ul>
+            <li>
+              <Link href="">Home</Link>
+            </li>
+            <li>
+              <Link href="">Community</Link>
+            </li>
+            <li>
+              <Link href="">Blog</Link>
+            </li>
+            <li>
+              <Link href="">Events</Link>
+            </li>
+          </ul>
+        </div>
 
-      <div>
-        <input type="text" placeholder="Search Here..." />
-        <button>Log in</button>
-      </div>
+        <div>
+          <input type="text" placeholder="Search Here..." />
+          <button>Log in</button>
+        </div>
+      </section>
     </nav>
   );
 };
